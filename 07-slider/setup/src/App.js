4168d6a14a4878cd91147d5c6686e5bf5c80ff14
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2023-01-04 22:20:24 +0400

    slider project completed

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import data from "./data";
+import Person from "./Person";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [index, setIndex] = useState(0);
+  const [people, setPeople] = useState(data);
+
+  useEffect(() => {
+    if (index < 0) {
+      setIndex(people.length - 1);
+    }
+    if (index > people.length - 1) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 1000);
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        <Person people={people} index={index} />
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
